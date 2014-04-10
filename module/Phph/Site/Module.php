586ee8fb0b60e2c159e4127db6e4d75f29067cc3
James Titcumb <james@asgrim.com>
2014-04-10 21:51:37 +0100

    Removed Application module as was not required any more

--- module/Phph/Site/Module.php
+++ module/Phph/Site/Module.php
@@ -4,9 +4,18 @@ namespace Phph\Site;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
 
 class Module implements AutoloaderProviderInterface, ConfigProviderInterface
 {
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
     public function getAutoloaderConfig()
     {
         return array(
