ddb6fcf26f45a472c179336f105c57920153829b
James Titcumb <james@asgrim.com>
2017-01-10 10:29:09 +0000

    Added third party authentication method to AuthenticationService implementation

--- src/App/Service/Authentication/Factory.php
+++ src/App/Service/Authentication/Factory.php
@@ -3,8 +3,10 @@ declare(strict_types = 1);
 
 namespace App\Service\Authentication;
 
+use App\Service\User\DoctrineFindUserByThirdPartyAuthentication;
 use App\Service\User\FindUserByEmailInterface;
 use App\Service\User\PhpPasswordHash;
+use Doctrine\ORM\EntityManagerInterface;
 use Interop\Container\ContainerInterface;
 use Zend\Authentication\Storage\Session;
 use Zend\ServiceManager\Factory\FactoryInterface;
@@ -23,8 +25,11 @@ class Factory implements FactoryInterface
         $requestedName,
         array $options = null
     ) : ZendAuthenticationService {
+        $entityManager = $container->get(EntityManagerInterface::class);
         return new ZendAuthenticationService(
             $container->get(FindUserByEmailInterface::class),
+            new DoctrineFindUserByThirdPartyAuthentication($entityManager),
+            $entityManager,
             new Session(),
             new PhpPasswordHash()
         );
