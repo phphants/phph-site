e1f5d06bd92739b9ac6aaa3a3095a459da6042a2
Jez Emery <jez.emery@gmail.com>
2017-10-06 19:49:16 +0100

    Make the checking in process a little more mobile friendly with anchors, and hiding irrelevant content

--- src/App/Action/Account/Meetup/CancelCheckInAction.php
+++ src/App/Action/Account/Meetup/CancelCheckInAction.php
@@ -56,6 +56,6 @@ final class CancelCheckInAction implements MiddlewareInterface
             $meetup->cancelCheckIn($user);
         });
 
-        return new RedirectResponse($this->urlHelper->generate('account-meetup-view', ['uuid' => $meetup->getId()]));
+        return new RedirectResponse($this->urlHelper->generate('account-meetup-view', ['uuid' => $meetup->getId()]).'#attendees');
     }
 }
