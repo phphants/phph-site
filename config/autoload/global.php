b4398203077ca482362653f505a7d02dd9a8fa8a
James Titcumb <james@asgrim.com>
2018-03-05 11:46:56 +0000

    Config defaults for S3

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,6 +4,15 @@ declare(strict_types=1);
 return [
     'phph-site' => [
         'meetups-data-path' => __DIR__ . '/../../data/meetups/',
+        's3' => [
+            'credentials' => [
+                'key' => 'POPULATE THIS',
+                'secret' => 'POPULATE THIS',
+            ],
+            'region' => 'eu-west-2',
+            'version' => 'latest',
+            'bucket' => 'POPULATE THIS',
+        ],
         'google-recaptcha' => [
             'api-url' => 'https://www.google.com/recaptcha/api/siteverify',
             'site-key' => 'POPULATE THIS',
