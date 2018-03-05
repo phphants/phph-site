0655432081f336fcc95304495f6d9f17adf6fe03
James Titcumb <james@asgrim.com>
2018-03-05 11:35:39 +0000

    Upload speaker headshots to S3 bucket using Flysystem

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 return [
     'phph-site' => [
         'meetups-data-path' => __DIR__ . '/../../data/meetups/',
-        'speaker-headshot-path' => __DIR__ . '/../../public/images/speakers/',
         'google-recaptcha' => [
             'api-url' => 'https://www.google.com/recaptcha/api/siteverify',
             'site-key' => 'POPULATE THIS',
--- src/App/Service/Speaker/MoveSpeakerHeadshotFactory.php
+++ src/App/Service/Speaker/MoveSpeakerHeadshotFactory.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Service\Speaker;
 
+use Aws\S3\S3Client;
 use Interop\Container\ContainerInterface;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
 use Zend\ServiceManager\Factory\FactoryInterface;
 
 /**
@@ -11,13 +14,27 @@ use Zend\ServiceManager\Factory\FactoryInterface;
  */
 final class MoveSpeakerHeadshotFactory implements FactoryInterface
 {
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param array|null $options
+     * @return FlysystemMoveSpeakerHeadshot
+     * @throws \InvalidArgumentException
+     * @throws \Psr\Container\ContainerExceptionInterface
+     * @throws \Psr\Container\NotFoundExceptionInterface
+     */
     public function __invoke(
         ContainerInterface $container,
         $requestedName,
         array $options = null
-    ) : MoveSpeakerHeadshot {
-        return new MoveSpeakerHeadshot(
-            $container->get('config')['phph-site']['speaker-headshot-path']
+    ) : FlysystemMoveSpeakerHeadshot {
+        return new FlysystemMoveSpeakerHeadshot(
+            new Filesystem(
+                new AwsS3Adapter(
+                    new S3Client($container->get('config')['phph-site']['s3']),
+                    $container->get('config')['phph-site']['s3']['bucket']
+                )
+            )
         );
     }
 }
